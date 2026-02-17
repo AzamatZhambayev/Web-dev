ea1336d4be756f2fd7e2d4094910c1104601fa11
Zhambo <azamat.zhambaev30@gmail.com>
2026-02-17 12:52:52 +0500

    Web-dev

--- angular-tutorial/src/app/app.ts
+++ angular-tutorial/src/app/app.ts
@@ -1,15 +1,3 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected readonly title = signal('angular-tutorial');
-}
 /*import {Component} from '@angular/core';
 
 @Component({
@@ -625,4 +613,3 @@ export class App {
   // TODO: Access form values with this.loginModel()
 }
  */
-
