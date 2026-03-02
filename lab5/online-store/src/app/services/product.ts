9fbc7dba8883dc0c6249cc34858cfb5ce3937a7a
Zhambo <azamat.zhambaev30@gmail.com>
2026-03-02 22:59:01 +0500

    Web-dev

--- lab5/online-store/src/app/services/product.ts
+++ lab5/online-store/src/app/services/product.ts
@@ -10,7 +10,8 @@ export class ProductService {
     { id: 2, name: 'Laptops' },
     { id: 3, name: 'Headphones' },
     { id: 4, name: 'Tablets' },
-    { id: 5, name: 'Cosmetics'}
+    { id: 5, name: 'Vacuuum Cleaner'},
+    { id: 6 ,name:"Toys"}
   ];
 
   products: Product[] = [
@@ -20,10 +21,10 @@ export class ProductService {
     { id:4,name:'Pixel 8',description:'Google phone',price:309900,rating:4.7,image:'assets/images/pix.jpg',link:'https://l.kaspi.kz/shop/6ThCpbaSaipoDD8',likes:0,categoryId:1},
     { id:5,name:'Huawei P60',description:'Huawei phone',price:250000,rating:4.5,image:'assets/images/hua.jpg',link:'https://l.kaspi.kz/shop/9kLr8y1aNsdqaRg',likes:0,categoryId:1},
 
-    { id:6,name:'MacBook Air',description:'Apple laptop',price:524333,rating:4.9,image:'assets/images/mac.jpg',link:'https://l.kaspi.kz/shop/DWCVwazJDW9MnWB',likes:0,categoryId:2},
-    { id:7,name:'Dell XPS',description:'Dell laptop',price:899990,rating:4.7,image:'assets/images/dell.jpg',link:'https://l.kaspi.kz/shop/Drb5qvcL97QALFY',likes:0,categoryId:2},
-    { id:8,name:'HP Spectre',description:'HP laptop',price:1299970,rating:4.6,image:'assets/images/spec.jpg',link:'https://l.kaspi.kz/shop/YefZ4vZTDWziLwQ',likes:0,categoryId:2},
-    { id:9,name:'Lenovo Yoga',description:'Lenovo laptop',price:550000,rating:4.5,image:'assets/images/lenova.jpg',link:'https://l.kaspi.kz/shop/HeURRDqW5m4Yaob',likes:0,categoryId:2},
+    { id:6,name:'MacBook Air',description:'Apple laptop',price:524333,rating:4.9,image:'assets/images/macair.jpg',link:'https://l.kaspi.kz/shop/DWCVwazJDW9MnWB',likes:0,categoryId:2},
+    { id:7,name:'Lenovo Legion 5',description:'Lenovo laptop',price:1690000,rating:4.7,image:'assets/images/legion.jpeg',link:'https://l.kaspi.kz/shop/6iLWKkqermWF3ZY',likes:0,categoryId:2},
+    { id:8,name:'Acer Nitro V 15',description:'Acer laptop',price:605870,rating:4.6,image:'assets/images/acer nitro.jpg',link:'https://l.kaspi.kz/shop/5SwQphRNcXb6YU6',likes:0,categoryId:2},
+    { id:9,name:'Lenovo Yoga',description:'Lenovo laptop',price:689990,rating:4.5,image:'assets/images/yoga.jpg',link:'https://l.kaspi.kz/shop/HeURRDqW5m4Yaob',likes:0,categoryId:2},
     { id:10,name:'Asus Zenbook',description:'Asus laptop',price:500000,rating:4.6,image:'assets/images/zen.jpg',link:'https://l.kaspi.kz/shop/A3MuqrkAZQKbCsH',likes:0,categoryId:2},
 
     { id:11,name:'AirPods Pro',description:'Apple earbuds',price:120000,rating:4.8,image:'assets/images/air.jpg',link:'https://l.kaspi.kz/shop/9M1AnwWNaPHnYp1',likes:0,categoryId:3},
@@ -38,11 +39,14 @@ export class ProductService {
     { id:19,name:'Lenovo Tab',description:'Lenovo tablet',price:200000,rating:4.4,image:'assets/images/lee.jpg',link:'https://l.kaspi.kz/shop/4xdiQW7t9311qXc',likes:0,categoryId:4},
     { id:20,name:'Xiaomi Pad',description:'Xiaomi tablet',price:220000,rating:4.5,image:'assets/images/oma.jpg',link:'https://l.kaspi.kz/shop/2xgxkTY1Hz6kTKC',likes:0,categoryId:4},
 
-    { id:21,name:'Sky High Mascara',description:'Lengthening mascara',price:7000,rating:4.8,image:'assets/images/high.jpg',link:'https://l.kaspi.kz/shop/9NL2WJb4k4T4UiN',likes:0,categoryId:5},
-    { id:22,name:'Fit Me Foundation',description:'Liquid foundation',price:6500,rating:4.7,image:'assets/images/me.jpg',link:'https://l.kaspi.kz/shop/6XdjZtqh6BYN3cs',likes:0,categoryId:5},
-    { id:23,name:'Matte Ink Lipstick',description:'Long lasting lipstick',price:6000,rating:4.6,image:'assets/images/ee.jpg',link:'https://l.kaspi.kz/shop/EBdoraRgTMEwdq6',likes:0,categoryId:5},
-    { id:24,name:'Nude Beauty Palette',description:'Eyeshadow palette',price:15000,rating:4.9,image:'assets/images/nude.jpg',link:'https://l.kaspi.kz/shop/3idPpkwGyP1hASV',likes:0,categoryId:5},
-    { id:25,name:'Rare Beauty Blush',description:'Liquid blush',price:12000,rating:4.8,image:'assets/images/rera.jpg',link:'https://l.kaspi.kz/shop/Gtxq1qpwyJ3purL',likes:0,categoryId:5},
+    { id:21,name:'TUMAR G PRO 200',description:'TUMAR VACUUMER',price:25000,rating:4.8,image:'assets/images/tumar.jpg',link:'https://l.kaspi.kz/shop/FJuUQV1r9y7x4cW',likes:0,categoryId:5},
+    { id:22,name:'Deerma DX118C',description:' Deerma VACUUMER',price:12000,rating:4.7,image:'assets/images/deerma.jpg',link:'https://l.kaspi.kz/shop/8gMpWLcEFzxzpBu',likes:0,categoryId:5},
+    { id:23,name:'LUMO Aqua X',description:'LUMO VACUUMER',price:220000,rating:4.6,image:'assets/images/luma.jpg',link:'https://l.kaspi.kz/shop/DMXtPJcbh1roa8q',likes:0,categoryId:5},
+    { id:24,name:' YOKU Kraft CL200',description:'YOKU VACUUMER',price:42500,rating:4.9,image:'assets/images/yoku.jpeg',link:'https://l.kaspi.kz/shop/7noqZd1GDqDP1he',likes:0,categoryId:5},
+    { id:25,name:'Rare Beauty Blush',description:'Dyson Cyclone V10 Absolute',price:237990,rating:4.8,image:'assets/images/dyson.jpg',link:'https://l.kaspi.kz/shop/2U7owcqthyWPtEB',likes:0,categoryId:5},
+
+    { id:26,name:'',description:'Dyson Cyclone V10 Absolute',price:237990,rating:4.8,image:'assets/images/dyson.jpg',link:'https://l.kaspi.kz/shop/2U7owcqthyWPtEB',likes:0,categoryId:6},
+    { id:27,name:'Rare Beauty Blush',description:'Dyson Cyclone V10 Absolute',price:237990,rating:4.8,image:'assets/images/dyson.jpg',link:'https://l.kaspi.kz/shop/2U7owcqthyWPtEB',likes:0,categoryId:5},
   ];
 
   getCategories(){ return this.categories; }
